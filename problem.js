542b5eb03ffc4411f9428e7bcb45b189263fcb59
Harish <arish4703@gmail.com>
2025-11-01 21:41:24 +0530

    problem definition was added

---
+++ problem.js
@@ -0,0 +1,12 @@
+
+//By adding 0.1 and 0.2,we exactly getfloting point numbers and it doesn't match with 0.3 when we use assignment operator. So it will returns false.
+console.log(0.1 + 0.2) //0.30000000000000004
+console.log(0.1 + 0.2 == 0.3)
+
+
+//Problem 2
+//Here we are comparing array with string. In this case array will be converted to string and then comparison will be done. So both are same and it will return true.
+const user = [1,2,3];
+const newUser = "1,2,3";
+
+console.log(user == newUser);
